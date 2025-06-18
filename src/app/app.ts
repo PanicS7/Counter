f3b17835a7c7d8cb82795c2eb36f79ecced2164a
Sanja Panic <gamesg274@gmail.com>
2025-06-18 15:37:03 +0200

    added button elements and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Counter } from "./counter/counter";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Counter],
+  imports: [Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './counter.css'
 })
 export class Counter {
-
 }
